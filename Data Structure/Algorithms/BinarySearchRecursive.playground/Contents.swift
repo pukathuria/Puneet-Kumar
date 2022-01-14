852d6ed50249581fe7a05245bb5a5465f5167bf8
Puneet Kumar <puneetkathuria90@gmail.com>
2022-01-14 18:06:26 +0530

    Binary Search Recursion Approach

--- Data Structure/Algorithms/BinarySearchRecursive.playground/Contents.swift
+++ Data Structure/Algorithms/BinarySearchRecursive.playground/Contents.swift
@@ -1,28 +1,37 @@
-import UIKit
 
-func binarySearch<T: Comparable>(_ array: [T], searchElement: T, range: Range<Int>) -> Int? {
-    if range.lowerBound >= range.upperBound {
-        
-        return nil
+// Created by Puneet Kumar on 14/01/22.
 
-    } else {
-        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
+// Recursive solution
 
-        // search left half
-        if array[midIndex] > searchElement {
-            return binarySearch(array, searchElement: searchElement, range: range.lowerBound ..< midIndex)
+func binarySearch(array: [Int], target: Int) -> Int {
 
-        // search right half
-        } else if array[midIndex] < searchElement {
-            return binarySearch(array, searchElement: searchElement, range: midIndex + 1 ..< range.upperBound)
-
-        // found the element
-        } else {
-            return midIndex
-        }
-    }
+var initialvalue = 0
+var endValue = array.count - 1
+return recursiveSearch(array: array, target: target, startingPoint: &initialvalue, endPoint: &endValue)
 }
 
-let numbers = [4,12,23,38,43,56,67,77,85,98]
+func recursiveSearch(array: [Int], target: Int, startingPoint: inout Int, endPoint: inout Int)-> Int
+{
+while (startingPoint <= endPoint)
+   {
+let mid = (startingPoint + endPoint)/2
+let midValue = array[mid]
+if (target == midValue)
+{
+   return mid
+}
+else if(target < midValue)
+{
+    endPoint = mid - 1
+}
+else
+{
+       startingPoint = mid + 1
+}
+}
+return -1
+}
 
-binarySearch(numbers, searchElement: 85, range: 0 ..< numbers.count)
+let arrSearch = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+
+binarySearch(array: arrSearch, target: 33)
